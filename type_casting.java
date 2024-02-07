ce5fc1de96fe75d2d2c020bb0cc78fdfe13a42cf
Rohit Mahato <83776656+r-mahato11@users.noreply.github.com>
2024-02-08 00:09:16 +0530

    Create type_casting.java

---
+++ type_casting.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class type_casting{
+  public static void main (String[]args){
+    Scanner sc = new Scanner(System.in);
+    float a = 25.12f;
+    int b = (int) a;
+  }
+}  
